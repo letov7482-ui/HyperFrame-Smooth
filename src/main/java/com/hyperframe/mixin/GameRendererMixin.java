1b3a21b1d8f3fa219b3f861b2c75f2fb19140575
letov7482-ui <letov7482@gmail.com>
2026-09-07 14:27:52 +0300

    Implement GameRendererMixin to monitor frames

---
+++ src/main/java/com/hyperframe/mixin/GameRendererMixin.java
@@ -0,0 +1,25 @@
+package com.hyperframe.mixin;
+
+import com.hyperframe.HyperFrame;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.render.RenderTickCounter;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public class GameRendererMixin {
+
+    @Inject(
+            method = "render",
+            at = @At("HEAD")
+    )
+    private void hyperFrame$beginFrame(
+            RenderTickCounter tickCounter,
+            boolean tick,
+            CallbackInfo ci
+    ) {
+        HyperFrame.FRAME_MONITOR.beginFrame();
+    }
+}
