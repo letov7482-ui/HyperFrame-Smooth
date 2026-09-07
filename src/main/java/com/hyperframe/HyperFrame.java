009dbd040dfea8ff59ccb1b699d366ff32e41c65
letov7482-ui <letov7482@gmail.com>
2026-09-07 14:32:25 +0300

    Introduce FrameStatistics to HyperFrame
    Added FrameStatistics for monitoring frame data.

--- src/main/java/com/hyperframe/HyperFrame.java
+++ src/main/java/com/hyperframe/HyperFrame.java
@@ -1,6 +1,7 @@
 package com.hyperframe;
 
 import com.hyperframe.core.FrameMonitor;
+import com.hyperframe.core.FrameStatistics;
 import net.fabricmc.api.ClientModInitializer;
 
 public class HyperFrame implements ClientModInitializer {
@@ -8,8 +9,13 @@ public class HyperFrame implements ClientModInitializer {
     public static final FrameMonitor FRAME_MONITOR =
             new FrameMonitor();
 
+    public static final FrameStatistics FRAME_STATISTICS =
+            new FrameStatistics(FRAME_MONITOR);
+
     @Override
     public void onInitializeClient() {
-        System.out.println("[HyperFrame] Frame Stability Core initialized.");
+        System.out.println(
+                "[HyperFrame] Frame Stability Core initialized."
+        );
     }
 }
